544bd677b01babff92c9b0185dca060bd42eeebd
B1029008 <91184018+winter920730@users.noreply.github.com>
2025-03-20 15:52:30 +0800

    Create Main52.c

---
+++ ex3/Main52.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void func(int i, int* j) {
+    printf("in func, i = %d, j = %d\n", i,j);
+    int t = *j;
+    j =i;
+    i = t;
+    printf("in func, i = %d, j = %d\n",i, *j);
+}
+
+int main() {
+    int n = 1, m = 2;
+    func(&n, &m);
+    printf("in main, n = %d, m = %d\n", n, m);
+    return 0;
+}
